e98b808d9283c95a4e37991ddb94ce9ce98a3d8f
Moses Baek <moses.baek09@gmail.com>
2021-11-05 12:07:21 -0400

    like button working

---
+++ models/sessions.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+const User = require('../models/user.js')
+const Schema = mongoose.Schema
+
+const sessionSchema = new Schema({
+    currentUser:[User.schema]
+})
+
+const Session = mongoose.model('Session', sessionSchema)
+module.exports = Session;
