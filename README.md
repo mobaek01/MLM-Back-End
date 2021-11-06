4b7dedca4fd211470f7d12c1258ed52dce807e9f
SoNesky <gueronlj@gmail.com>
2021-11-06 09:43:35 -0400

    added a new sessions rout

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # MLM-Back-End
+
+##Issue: 'session' info is lost on refresh.
+##Solution??: Somehow store the session state in local storage.
+
+On login, user gains a session object
+
+Session = {
+   name: String,
+   online: Boolean
+}
+
+const = [online, setOnline] = useState('false')
+
+OnLogin => make axios call to findOneAndUpdate({username},
+   session:{
+      name:username from login form,
+      online:true
+   }
+   setOnline(true)
+   *On login, save online state to local storage
+
+OnLogout => make axios call to findOneAndUpdate({username},
+   session:{
+      name:Username,
+      online:false
+   }
+   setOnline(false)
+
+
+useEffect( retrieve online state from local storage )
+
+online ? show logout button : show login button
+------------------------------------------------------------------------------
+
+const checkStatus = (NAME) => {
+//find a session with this NAME;
+   if foundSession.online == true,
+      return true'
+   else
+      return false
+}
+
+checkStatus(username)? Print Welcome username! :     
--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -3,12 +3,13 @@ const bcrypt = require('bcrypt')
 const users = express.Router()
 const User = require('../models/user.js')
 
+//---------------Find all users------------------
 users.get('/', (req, res) => {
     User.find({}, (error, foundUser) => {
         res.json(foundUser)
     })
 })
-
+//==================Create User====================
 users.post('/', (req, res) => {
     req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10))
     User.create(req.body, (error, createdUser) => {
@@ -17,24 +18,24 @@ users.post('/', (req, res) => {
     })
 })
 
-// LOG IN
-users.put('/login/:username', (req, res) => {
-    User.findOneAndUpdate(
-        {username: req.params.username},
-        {online: true},
-        (error, changedStatus) => {
-            res.json(changedStatus)
-    })
-})
+//======================Log in==============================
+// users.put('/login/:username', (req, res) => {
+//     User.findOneAndUpdate(
+//         {username: req.params.username},
+//         {online: true},
+//         (error, changedStatus) => {
+//             res.json(changedStatus)
+//     })
+// })
 
-// LOG OUT
-users.put('/logout/:username', (req, res) => {
-    User.findOneAndUpdate(
-        {username: req.params.username},
-        {online: false},
-        (error, changedStatus) => {
-            res.json(changedStatus)
-    })
-})
+//=====================Log out================
+// users.put('/logout/:username', (req, res) => {
+//     User.findOneAndUpdate(
+//         {username: req.params.username},
+//         {online: false},
+//         (error, changedStatus) => {
+//             res.json(changedStatus)
+//     })
+// })
 
 module.exports = users;
